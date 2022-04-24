55e73e21b6d35e25ee7c770881f59e90b83abd30
sabrinamoraism <mariasabrinamorais@gmail.com>
2022-04-23 21:22:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bem-vindo(a) ao MCUflix :movie_camera::tv:
+### **O MCUflix é uma recriação da interface da Netfllix com as produções do Universo Cinematográfico Marvel.**
+
+##### Trata-se de um desafio de projeto proposto no bootcamp Carrefour Web Developer.
---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+})
